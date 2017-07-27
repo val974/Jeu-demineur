567a8d3a867588c230a11c7daa0a020ca7e21ddc
val974 <valerie.dij@gmail.com>
2017-07-27 17:43:34 +0400

    changement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
+
+
 let cartes = document.querySelectorAll('img');
 let tableauID = [];
 let point =0;
+
 
 for (let i =0; i<cartes.length;i++){ //je parcours mon tableau
 	let id = cartes[i].id;
 	tableauID.push(cartes[i].id);//je mets les id des cartes dans un tableau
 }
 
-setInterval(nombreAleatoire,1000); //je répète ma fonction 
+let bouton1 = document.querySelector("#button1");
+bouton1.addEventListener("click",function top(){
+
+let debut = setInterval(nombreAleatoire,1000); //je répète ma fonction 
 
 function nombreAleatoire(){
 	let chiffre = Math.round(Math.random() * 8);//je choisis un nombre aléatoire
 		let changement = tableauID[chiffre];//l'ordinateur choisit un id aléatoirement
 		cartes[changement].setAttribute("src","img/bombe.jpg"); //je remplace la source de l'id choisit par l'ordinateur
 		cartes[changement].addEventListener("click", score); //j'ajoute à cet id un évènement clic
-		setTimeout(function(){cartes[changement].setAttribute("src","img/pattern.png")},550)//je retourne les cartes au bout d'un certain temps
+		setTimeout(function(){cartes[changement].setAttribute("src","img/pattern.png")},1000)//je retourne les cartes au bout d'un certain temps
 	}
 
 
@@ -23,12 +29,12 @@ function score(e){
 	document.getElementById("pointsgagnes").innerHTML =  " Votre score est de : " + point;
 	e.currentTarget.removeEventListener("click", score); //j'enlève l'évènement clic pour qu'il ne garde pas le score en mémoire
 }
+});
 
 
-
-
-
-
+let bouton2 = document.querySelector("#button2");
+bouton2.addEventListener("click",function(){
+});
 
 
 
