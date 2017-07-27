f7cc2a4fdbc745ea37792ceb61261a53d1e4df4e
val974 <valerie.dij@gmail.com>
2017-07-27 16:57:33 +0400

    le jeu fonctionne

--- js/script.js
+++ js/script.js
@@ -1,76 +1,36 @@
 let cartes = document.querySelectorAll('img');
-let point = 0;
 let tableauID = [];
-let chiffre = 0;
-let tableauChoixOrdi=[];
+let point =0;
 
-for (let i =0; i<cartes.length;i++){
+for (let i =0; i<cartes.length;i++){ //je parcours mon tableau
 	let id = cartes[i].id;
-	tableauID.push(cartes[i].id);
+	tableauID.push(cartes[i].id);//je mets les id des cartes dans un tableau
 }
 
-setInterval(nombreAleatoire,1000);
+setInterval(nombreAleatoire,1000); //je répète ma fonction 
 
 function nombreAleatoire(){
-	let chiffre = Math.round(Math.random() * 8);
-	let position = cartes[chiffre].setAttribute("src","img/bombe.jpg");
-	tableauChoixOrdi.push(cartes[chiffre]);
-	if(tableauChoixOrdi.length===1){
-		tableauChoixOrdi[0].addEventListener("click", score);
-		setTimeout(function(){cartes[chiffre].setAttribute("src","img/pattern.png")},550);
-		/*tableauChoixOrdi[0].removeEventListener("click",score);*/
-		tableauChoixOrdi=[];
-
-
+	let chiffre = Math.round(Math.random() * 8);//je choisis un nombre aléatoire
+		let changement = tableauID[chiffre];//l'ordinateur choisit un id aléatoirement
+		cartes[changement].setAttribute("src","img/bombe.jpg"); //je remplace la source de l'id choisit par l'ordinateur
+		cartes[changement].addEventListener("click", score); //j'ajoute à cet id un évènement clic
+		setTimeout(function(){cartes[changement].setAttribute("src","img/pattern.png")},550)//je retourne les cartes au bout d'un certain temps
 	}
-}
 
-function score(){
-		point+=5;
-		console.log(point);
-		document.getElementById("pointsgagnes").innerHTML =  " Votre score est de : " + point;
-	/*}*/
-}
-
-
-/*
-let cartes = document.querySelectorAll('img');
-console.log(cartes);
-let point = 0;
-let tableauID = [];
-let chiffre = 0;
-let tableauChoixOrdi=[];
-/*let point =0;
-
-for (let i =0; i<cartes.length;i++){
-	let id = cartes[i].id;
-	tableauID.push(cartes[i].id);
-}
-
-setInterval(nombreAleatoire,500);
-
-function nombreAleatoire(e){
-	let chiffre = Math.round(Math.random() * 8);
-	for (let j=0;j<tableauID.length;j++){
-		let changement = tableauID[chiffre];
-		let position = cartes[changement].setAttribute("src","img/bombe.jpg");
-		tableauChoixOrdi.push(cartes[changement])
-		tableauChoixOrdi=[];
-		cartes[changement].addEventListener("click", score);
-		/*e.currentTarget.addEventListener("click", score);
-		setTimeout(function(){cartes[changement].setAttribute("src","img/pattern.png")},550)
-	}
-}
-
-nombreAleatoire();
 
 function score(e){
-	if(tableauChoixOrdi.length<1){
-		point+=5;
-	}
+		point++;//j'incrèmente les points au fur et à mesure
 	document.getElementById("pointsgagnes").innerHTML =  " Votre score est de : " + point;
+	e.currentTarget.removeEventListener("click", score); //j'enlève l'évènement clic pour qu'il ne garde pas le score en mémoire
 }
-*/
+
+
+
+
+
+
+
+
 
 
 /*function nombreAleatoire(e){
